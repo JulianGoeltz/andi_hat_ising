fd6f1754ea531d6f9800334783ea5a112c70590f
JulianGoeltz <goeltz@stud.uni-heidelberg.de>
2020-10-22 00:34:10 +0200

    no interaction yet, but levels can be won, and the game

--- bm_game/game.py
+++ bm_game/game.py
@@ -16,14 +16,37 @@ class Game:
         self.levels = [
             [(1, 1, 0), ],
             [(1, 1, 1), ],
+            [(0, 1, 0), (1, 1, 0), (2, 1, 0), ],
+            [(1, 0, 1), (1, 1, 1), (1, 2, 1), ],
         ]
         self.level_no = 0
 
         self.display = displaying.Display(size, size)
-        self.ising = ising.IsingModel(size, bias=0.)
+        self.ising = ising.IsingModel(size, bias=-2.)
 
+        # resetting and first display
         self.reset_field()
         self.set_up_level()
+        self.force_spins()
+        states = self.ising.get_rectangular_states()
+        self.display.update(states, forced=(self.forced_spins > -1))
+
+    def check_win(self):
+        """determine whether target satisfied, if so upgrade lvl"""
+        to_check = self.ising.get_rectangular_states()[self.size // 2 - 2:, self.size // 2 - 2:]
+        to_check = to_check[:3, :3]
+        # print(self.ising.get_rectangular_states())
+        # print(to_check)
+        # time.sleep(400)
+        winning = np.all(to_check[self.target > -1] == self.target[self.target > -1])
+        if not winning:
+            return
+        self.level_no += 1
+        if self.level_no == len(self.levels):
+            print("YOU BESTED THE GAME, no more levels. congrats")
+            sys.exit()
+        self.set_up_level()
+        self.display.won_level()
 
     def reset_field(self):
         """unforce spins"""
@@ -47,14 +70,18 @@ class Game:
 
     def run(self):
         """general procedure"""
-        print("Starting main loop")
+        print("Starting the game")
         iteration = 0
         while True:
             iteration += 1
             # print(f"start of loop {iteration}")
-            states = self.ising.get_rectangular_states()
-            self.display.update(states)
+
             self.ising.update()
+            self.force_spins()
+            states = self.ising.get_rectangular_states()
+            self.display.update(states, forced=(self.forced_spins > -1))
+            self.check_win()
+
             time.sleep(1)
 
             if iteration == 1000:
